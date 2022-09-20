13d796b62d3c5f7fdebe85ad68166b338b40c923
andy <13021013096@163.com>
2022-09-20 23:23:49 +0800

    创建项目、提交自定义UICollectionViewFallLayout

--- AYTV/AYTV/Classes/Main/Controller/MainViewController.swift
+++ AYTV/AYTV/Classes/Main/Controller/MainViewController.swift
@@ -11,19 +11,22 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addChildVc(name: "Home")
+        addChildVc(name: "Rank")
+        addChildVc(name: "Discover")
+        addChildVc(name: "Profile")
 
-        // Do any additional setup after loading the view.
     }
     
+    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension MainViewController {
+    
+    fileprivate func addChildVc(name: String) {
+        let homeVc = UIStoryboard(name: name, bundle: nil).instantiateInitialViewController()!
+        addChild(homeVc)
     }
-    */
-
+    
 }
